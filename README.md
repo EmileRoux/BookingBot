bfcd429e01d03b492e59d7014a0439cbb0df128a
EmileRoux <siremileroux@gmail.com>
2021-12-24 01:34:10 +0200

    Add faunadb update README

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A simple whatsapp bot for a restaurant
 
 ## Netlify
 
+Requires "FAUNADB_SERVER_SECRET" environment variable for Fauna
+
 ## Gushup
 
-## Firebase
+## Fauna
--- src/lambda/bookingbot.js
+++ src/lambda/bookingbot.js
@@ -1,22 +1,52 @@
-export async function handler(event, context) {
+ /* Import faunaDB sdk */
+ const faunadb = require('faunadb')
 
-    if(event.httpMethod == "POST") {
-        try {
-          let body = JSON.parse(event.body);
-          console.log(body);
-
-          return {
-            statusCode: 200,
-            body: ""
-          };
-
-        } catch(e) {
-          console.error(e);
-        }
-    }
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello world ${Math.floor(Math.random() * 10)}` })
-    };
-}
+ /* configure faunaDB Client with our secret */
+ const q = faunadb.query
+ const client = new faunadb.Client({
+   secret: process.env.FAUNADB_SERVER_SECRET,
+   keepAlive: false
+ })
+ 
+ export async function handler(event, context) {
+ 
+     if(event.httpMethod == "POST") {
+         try {
+           let body = JSON.parse(event.body);
+           console.log(body);
+ 
+           return client.query(q.Paginate(q.Match(q.Ref('indexes/all_commands'))))
+           .then((response) => {
+             const commands = response.data
+           
+             const getAllDataQuery = commands.map((ref) => {
+               return q.Get(ref)
+             })
+ 
+             console.log(getAllDataQuery);
+ 
+             // then query the refs
+             return client.query(getAllDataQuery).then((ret) => {
+               return {
+                 statusCode: 200,
+                 body: ""
+               }
+             })
+           }).catch((error) => {
+             console.log('error', error)
+             return {
+               statusCode: 400,
+               body: JSON.stringify(error)
+             }
+           })
+ 
+         } catch(error) {
+           console.log('error', error)
+         }
+     }    
+ 
+     return {
+       statusCode: 200,
+       body: JSON.stringify({ message: 'Pong' })
+     };
+ }
